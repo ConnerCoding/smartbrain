b09a03ea5e6d581af2dd37acb4065d379a12e0b9
Conner Schiller <conner.schiller@gmail.com>
2020-07-16 17:33:12 -0700

    added facerecognition.js description longer than the code itself :)

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './FaceRecognition.css';
 
+/** 
+Display the box on the face in the image
+First you display the image with imageUrl
+Then you use the box parameter which is 
+the object you get from the displayFaceBox
+function, which gets its data from calcFaceLocation
+which gets its data from the Clarifai API call in the
+onPictureSubmit function :)
+*/ 
+
 const FaceRecognition = ({imageUrl, box}) => {
     return (
         <div className='center ma'>
